fa91455f3b7d20bbabb0f0ad60713b25c9dade84
JunilSeo <dccrdseo@naver.com>
2025-04-16 15:49:15 +0900

    SwiftUI Tutorials - Creating and combining views

---
+++ Landmarks/Landmarks/CircleImage.swift
@@ -0,0 +1,30 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by 서준일 on 4/16/25.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+        // Cicle 타입은 mask로 사용할 수 있는 도형이며, storke, fill등 을 지정해 하나의 뷰로도 사용할 수 있음
+        // 이미지가 Circle안에 존재하는 화면의 구성은
+        // Image -> Clipshape -> Circle 순서로 화면 위에 그려지기 때문
+            .clipShape(Circle())
+        // 흰색 테두리를 가진 원을 하나 만들고, overlay로 이미지 위에 추가하여 이미지에 테두리를 줌
+        // 그 다음 이미지에 그림자를 추가한다
+        // 중요한 것은 overlay에 그림자가 추가되는 것이 아니고 이미지 자체에 추가되는 것
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+        
+    }
+}
+
+#Preview {
+    CircleImage()
+}
--- Landmarks/Landmarks/LandmarksApp.swift
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -7,6 +7,10 @@
 
 import SwiftUI
 
+// SwiftUI 앱 생명 주기를 사용하는 앱은 App 프로토콜을 준수하는 구조체를 가짐
+// body 프로퍼티는 하나 이상의 Scene을 반환하며, 각 씬은 화면에 표시할 콘텐츠를 제공함
+// @main 속성은 앱의 진입점 (entry point)을 나타냄
+
 @main
 struct LandmarksApp: App {
     var body: some Scene {
